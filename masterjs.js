f92f61b57eb60a3280d90ee7d79215ee98d15ad0
FelixLee <146620916+anomalousClasher@users.noreply.github.com>
2024-01-28 20:30:07 +0800

    Add files via upload

---
+++ masterjs.js
@@ -0,0 +1,15 @@
+
+function github_redirect() {
+    window.open('https://github.com/anomalousClasher');
+
+}
+
+function linkedin_redirect() {
+    window.open('https://www.linkedin.com/in/yin-hui-lee-2a2213261');
+
+}
+
+
+
+
+
